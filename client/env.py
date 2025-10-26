21104f642ad2b7a0b932b735fa22f472a6996c25
naresh-venkatachalam <nareshvenkat14@gmail.com>
2025-10-26 15:10:06 +0530

    Update env.py

--- client/env.py
+++ client/env.py
@@ -1,15 +1,26 @@
-OPENAI_API_KEY="YOURE_API_KEY_HERE"
-ANTHROPIC_API_KEY="YOURE_API_KEY_HERE"
-
-
-AWS_REGION='us-east-1'  # Or your preferred region
+# ------------------------------------------------------------------------------------------------
+#  Provider Configuration
+# ------------------------------------------------------------------------------------------------
 LLM_PROVIDER='bedrock' # openai, anthropic, bedrock
 
-BEDROCK_MODEL_ID='amazon.nova-micro-v1:0:128k'
+
+# ------------------------------------------------------------------------------------------------
+#  Provider Specific Configuration
+# ------------------------------------------------------------------------------------------------
+OPENAI_API_KEY="YOURE_API_KEY_HERE"
+ANTHROPIC_API_KEY="YOURE_API_KEY_HERE"
+BEDROCK_MODEL_ID='claude_3_5_sonnet_v2'  # Claude 3.5 Sonnet v2
 
 
-# Option 2: Assume Role (with external ID support)
-# Uncomment and fill in to assume a role instead of using direct credentials
-AWS_ROLE_ARN='AWS_ROLE_ARN_HERE'
-AWS_EXTERNAL_ID='AWS_EXTERNAL_ID_HERE'  # Required if role trust policy specifies ExternalId
-# AWS_ROLE_SESSION_NAME='weather-app-session'  # Optional, defaults to 'weather-app-session'
+# ------------------------------------------------------------------------------------------------
+#  AWS Region Configuration
+# ------------------------------------------------------------------------------------------------
+AWS_REGION='us-east-1'  # Or your preferred region
+
+
+# ------------------------------------------------------------------------------------------------
+#  Assume Role Configuration
+# ------------------------------------------------------------------------------------------------
+AWS_ROLE_ARN='<YOUR_ROLE_ARN>'
+AWS_EXTERNAL_ID='<YOUR_EXTERNAL_ID>'
+AWS_ROLE_SESSION_NAME='weather-app'  # Optional, defaults to 'weather-app-session'
